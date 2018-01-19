42ace2f4c934c07a64596a8faefaeb04aac9dbe5
YecaPee <yecapee@gmail.com>
2018-01-19 22:38:27 +0800

    1

--- spacewar.js
+++ spacewar.js
@@ -6,7 +6,7 @@
   var renCount = 0;
   var renderTime = 500;
   var bulletTime = 200;
-  var moveTime = (window.innerWidth < 500) ? 10 : 80;
+  var moveTime = (window.innerWidth < 500) ? 120 : 80;
   var nextPoling = 1;
   var killCount = 0;
 
