3363356c0dd4ed14d042847cdf9a5aa7274b393d
panpanpan <yecapee@gmail.com>
2018-03-20 16:44:28 +0800

    -

--- js/lookPath.js
+++ js/lookPath.js
@@ -1,5 +1,20 @@
 import { w } from './config';
-import { ezPosition } from './positionMethod';
+import {
+  ezPosition,
+  positionTosXsY,
+  sXsYToPosition,
+} from './positionMethod';
+
+function circle(x, y, r, all, now, margin) {
+  var ang = Math.PI * 2 / all;
+  //var _margin = 2 * Math.PI / 360 * (margin || 0);
+  var _margin = 0;
+  var _x = Math.round(x + r * Math.cos(ang * now + _margin));
+  var _y = Math.round(y + r * Math.sin(ang * now + _margin));
+  // console.log(x,y,_x,_y);
+  return sXsYToPosition(_x, _y);
+}
+
 export default {
   'zark': function (ps) {
     var rs = [ps];
@@ -37,27 +52,27 @@ export default {
     }
     return rs;
   },
-  'SPIDER':function(ps){
+  'SPIDER': function (ps) {
     var xy = ezPosition(ps);
     var rs = [xy(0, 0)];
     if (ps % w !== w - 1) {
-      rs = [...rs,xy(1, -1),xy(1, -2),xy(1, 1)];
+      rs = [...rs, xy(1, -1), xy(1, -2), xy(1, 1)];
     }
     if (ps % w !== 0) {
-      rs = [...rs,xy(-1, -1),xy(-1, -2),xy(-1, 1)];
+      rs = [...rs, xy(-1, -1), xy(-1, -2), xy(-1, 1)];
     }
-    return rs;     
+    return rs;
   },
-  'TALONS':function(ps){
+  'TALONS': function (ps) {
     var xy = ezPosition(ps);
-    var rs = [xy(0, -2),xy(0, 1)];
+    var rs = [xy(0, -2), xy(0, 1)];
     if (ps % w !== w - 1) {
-      rs = [...rs,xy(1, 0),xy(2, -1),xy(2, -2),xy(2, -3)];
+      rs = [...rs, xy(1, 0), xy(2, -1), xy(2, -2), xy(2, -3)];
     }
     if (ps % w !== 0) {
-      rs = [...rs,xy(-1, 0),xy(-2, -1),xy(-2, -2),xy(-2, -3)];
+      rs = [...rs, xy(-1, 0), xy(-2, -1), xy(-2, -2), xy(-2, -3)];
     }
-    return rs;     
+    return rs;
   },
   'MK-1': function (ps) {
     var rs = [ps, ps + w, ps + 3 * w];
@@ -107,4 +122,14 @@ export default {
     }
     return rs;
   },
+  'CIRCLE': function (ps) {
+    //var p0 = ezPosition(ps);
+    var p0 = positionTosXsY(ps);
+    var pointLength = 20;
+    var rs = [];
+    for (var i = 0; i <= pointLength; i++) {
+      rs.push(circle(p0.x, p0.y, 5, pointLength, i, 0));
+    }
+    return rs;
+  }
 };
